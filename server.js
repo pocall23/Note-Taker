be7bf50f7c9d25d0a91dc8f5f56df75cb85fa006
Peter O'Callaghan <peterocallaghan23@gmail.com>
2022-07-20 09:47:47 +1000

    changed script

--- server.js
+++ server.js
@@ -1,45 +1,36 @@
-const fs = require("fs");
-const express = require("express");
-const path = require("path");
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-//use files from public folder
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 
-//route to notes.html
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-//route to read the `db.json` file 
-app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
-});
-
-//route to index.html 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-//add new note to db.json and return it to user
-app.post("/api/notes", (req, res) => {
-    let newNote = req.body;
-    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let notelength = (noteList.length).toString();
-    
-    newNote.id = notelength;
-    noteList.push(newNote);
-
-    //write the updated data to db.json
-    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
-    res.json(noteList);
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'db/db.json'))
 })
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
 
-app.listen(PORT, () => console.log("Server listening on port " + PORT));
+app.post('/api/notes' ,(req, res) => {
+    let newNote =req.body;
+    let notes =  JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
+    let notesLength = (notes.length).toString();
+
+    newNote.id = notesLength;
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json(notes)
+})
+
+app.listen(PORT, () => console.log('listening on port '+ PORT));
